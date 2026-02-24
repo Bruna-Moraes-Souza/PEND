1432e06d80579c0567cff7ecc23b6d6afae0826d
Bruna-Moraes-Souza <bruna.m.souza9@alunosenai.br>
2026-02-24 15:57:29 -0300

    tom

---
+++ JAVASCRIPT/Dom/Exemplo.js
@@ -0,0 +1,7 @@
+document.getElementById("conteudo").innerHTML = "<p>Olá, Mundo Dom</p>"
+document.getElementById("mensagem").textContent="Texto simples, sem HTML TEXT CONTENT";
+document.getElementById("foto").setAttribute("src", "image.png");
+let url = document.getElementById("link").setAttribute("href");
+console.log(url);
+document.getElementById("caixa").style.backgroundColor = "lightblue";
+document.getElementById("aleta").classList.add="destaque";
---
+++ JAVASCRIPT/Dom/script.js
@@ -0,0 +1,3 @@
+function Mostrarmensagem(){
+    alert ("Olá, Aluno");
+}
